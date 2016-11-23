39517a160996816660b71b78641d4cbd370c493a
Andre Price <obsoleted@users.noreply.github.com>
2016-11-22 17:46:27 -0800

    JSHTTP: Remove extra comments

--- JSHttpTrigger/index.js
+++ JSHttpTrigger/index.js
@@ -1,22 +1,8 @@
 module.exports = function(context, req) {
     context.log('JS_ HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-    // context.log(JSON.stringify(context));
-    context.log("req");
-    context.log(req);
-    context.log(context.req);
-    context.log('---');
-    context.log(req.params);
-    context.log(req.params.id);
-    context.log(req.params['id']);
-    context.log('---');
-
-    context.log(context.req.params);
-    context.log(context.req.params.id);
-    context.log(context.req.params['id']);
     context.res = {
         status: 200,
         body: `Id ${req.params.id} ${ req.params.activearg || true }`
     }
-
     context.done();
 };
