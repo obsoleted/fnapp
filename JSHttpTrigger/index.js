78245acd5aab7496b3ff8c920ddd45cf6499fad6
Andre Price <obsoleted@users.noreply.github.com>
2016-11-22 17:17:21 -0800

    JSHTTP: Try getting at param

--- JSHttpTrigger/index.js
+++ JSHttpTrigger/index.js
@@ -1,6 +1,9 @@
 module.exports = function(context, req) {
     context.log('JS HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
     context.log(JSON.stringify(context));
+    context.log(req.params);
+    context.log(req.params.id);
+    context.log(req.params['id']);
 
     context.res = {
         status: 200,
