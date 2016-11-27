853a90045a545d1f8d5b58d78b37a77e16e0f258
Andre Price <obsoleted@users.noreply.github.com>
2016-11-26 19:17:50 -0800

    WIP: JS Echo

---
+++ JSHttpEcho/index.js
@@ -0,0 +1,8 @@
+module.exports = function(context, req) {
+    context.log('JS_ HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+    context.res = {
+        status: 200,
+        body: `Id ${req.params.id} ${ req.params.activearg || true }`
+    }
+    context.done();
+};
